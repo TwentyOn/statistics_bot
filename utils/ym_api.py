6db1efa69c8eb225a0e9d5c87d9dfd782b4ef18b
Aleksey <aleksey-000@mail.ru>
2025-11-05 17:29:31 +0300

    минимальная дата начала периода теперь жестко зафиксирована на 01.01.2020 для всех счетчиков

--- utils/ym_api.py
+++ utils/ym_api.py
@@ -70,6 +70,7 @@ class YMRequest:
         self.token = oauth_token
         self.api_url = 'https://api-metrika.yandex.net/stat/v1/data'
         self.headers = {'Authorization': self.token}
+        self.min_date = datetime.date(2020, 1, 1)
 
     async def _get_counter(self, raw_url: str):
         # домен
@@ -77,24 +78,25 @@ class YMRequest:
         # сессия базы данных
         async with async_session_maker() as session:
             domain_counter_obj = await session.execute(
-                select(DomainCounter.counter, DomainCounter.created_at).where(DomainCounter.domain_name == netloc))
+                select(DomainCounter.counter).where(DomainCounter.domain_name == netloc))
         # получаем счётчик по домену
-        data = domain_counter_obj.first()
-        if not data:
+            counter = domain_counter_obj.scalar_one()
+        if not counter:
             raise BadRequestError(
                 f'Не удалось найти счётчик Яндекс Метрики по домену: {netloc}.'
             )
-        counter, created_at = data
-        return counter, created_at
+        return counter
 
     async def _get_counters(self, raw_urls: list[str]):
         # домены из сырых URL
         netlocs = map(lambda url: urlparse(url).netloc, raw_urls)
 
         async with async_session_maker() as session:
-            smtm = select(DomainCounter.counter, DomainCounter.created_at).where(DomainCounter.domain_name.in_(netlocs))
+            smtm = select(DomainCounter.counter).where(DomainCounter.domain_name.in_(netlocs))
             domain_counters_objects = await session.execute(smtm)
-        return domain_counters_objects.all()
+            counters = domain_counters_objects.scalars().all()
+        print(counters)
+        return counters
 
     def statistic_placeholder(self, stat, raw_url=None):
         stat = statistic(
@@ -105,11 +107,11 @@ class YMRequest:
 
     async def get_statistics(self, session, raw_url, cleaned_url, date1, date2):
         # await global_limiter.acquire()
-        counter_id, created_at = await self._get_counter(raw_url)
+        counter_id = await self._get_counter(raw_url)
 
         # если дата начала периода < даты создания счётчика, берём дату создания счётчика
-        if date1 is None or datetime.datetime.strptime(date1, '%Y-%m-%d').date() < created_at:
-            date1 = str(created_at)
+        if date1 is None or datetime.datetime.strptime(date1, '%Y-%m-%d').date() < self.min_date:
+            date1 = str(self.min_date)
         if date2 is None:
             date2 = str(datetime.date.today())
 
@@ -121,9 +123,10 @@ class YMRequest:
             'date2': date2,
             'accuracy': 'full'
         }
+        print('single', parameters)
 
         async with self.semaphore:
-            print(datetime.datetime.now().time(), 'выполняю запрос')
+            print(datetime.datetime.now().time(), 'запрос', cleaned_url)
             async with session.get(self.api_url, headers=self.headers, params=parameters) as response:
                 if response.status == 400:
                     error = await response.json()
@@ -137,7 +140,7 @@ class YMRequest:
                     raise BadRequestError(f'Не удалось получить данные от API Яндекс Метрики: {message}')
                 stat = await response.json()
                 stat = stat.get('data')
-
+        print(datetime.datetime.now().time(), 'ответ', cleaned_url)
         if stat:
             # т.к группировки не используются всегда берём первый элемент из data
             stat = stat[0]['metrics']
@@ -152,13 +155,10 @@ class YMRequest:
         return stat
 
     async def get_sum_statistics(self, raw_urls, cleaned_urls, date1, date2):
-        counters_data = await self._get_counters(raw_urls)
-        counters = map(lambda t: t[0], counters_data)
-        created_at = map(lambda t: t[1], counters_data)
+        counters = await self._get_counters(raw_urls)
         counter_ids = ','.join(counters)
-        min_date = min(created_at)
-        if date1 is None or datetime.datetime.strptime(date1, '%Y-%m-%d').date() < min_date:
-            date1 = str(min_date)
+        if date1 is None or datetime.datetime.strptime(date1, '%Y-%m-%d').date() < self.min_date:
+            date1 = str(self.min_date)
         if date2 is None:
             date2 = str(datetime.date.today())
         filters = ' OR '.join([f"EXISTS(ym:pv:URL=*'*{cleaned_url}*')" for cleaned_url in cleaned_urls])
@@ -170,6 +170,7 @@ class YMRequest:
             'date2': str(date2),
             'accuracy': 'full'
         }
+        print('sum', parameters)
         stat = requests.get(self.api_url, headers=self.headers, params=parameters)
         stat = stat.json().get('data')
         if stat:
